d800376f9e104cfe97bfe790b752236e8f93a822
andrzej-sabat <37102669+andrzej-sabat@users.noreply.github.com>
2019-04-08 18:51:41 +0200

    Model Subject

---
+++ Dziennik/src/main/java/Modele/Subject.java
@@ -0,0 +1,61 @@
+package Modele;
+
+
+
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table
+public class Subject {
+
+    private long subjectId;
+    private String subjectName;
+    private Set<Classes> classes;
+    private Set<Grades> grades;
+
+    @Id
+    @GeneratedValue
+    @Column(name = "subject_id")
+    public long getSubjectId(){
+        return this.subjectId;
+    }
+    public void setSubjectId(long subjectId){
+        this.subjectId = subjectId;
+    }
+
+    @Column(name = "subject_name", nullable = false, length = 50)
+    public String getSubjectName(){
+        return this.subjectName;
+    }
+    public void setSubjectName(String subjectName){
+        this.subjectName = subjectName;
+    }
+
+    @OneToMany(mappedBy = "subject", cascade = CascadeType.ALL)
+    public Set<Grades> getGrades() {
+        return grades;
+    }
+    public void setGrades(Set<Grades> grades) {
+        this.grades = grades;
+    }
+
+    @ManyToMany(fetch = FetchType.EAGER, mappedBy = "subjects")
+    public Set<Classes> getClasses() {
+        return classes;
+    }
+    public void setClasses(Set<Classes> classes) {
+        this.classes = classes;
+    }
+
+    @Override
+    public String toString() {
+        return "Subject{" +
+                "subjectId=" + subjectId +
+                ", subjectName='" + subjectName + '\'' +
+                ", classes=" + classes +
+                ", grades=" + grades +
+                '}';
+    }
+}
