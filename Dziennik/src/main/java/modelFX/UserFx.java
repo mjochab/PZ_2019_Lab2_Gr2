4fcb69593c28c833c8ded23ee2239f30fa412cd2
andrzej-sabat <37102669+andrzej-sabat@users.noreply.github.com>
2019-04-24 21:18:12 +0200

    I19L2GR2-38 TeacherFx, UserFx, WarnsFx

--- Dziennik/src/main/java/modelFX/UserFx.java
+++ Dziennik/src/main/java/modelFX/UserFx.java
@@ -1,4 +1,96 @@
 package modelFX;
 
+import javafx.beans.property.*;
+
 public class UserFx {
+    private LongProperty userId = new SimpleLongProperty();
+    private StringProperty username = new SimpleStringProperty();
+    private StringProperty passwrd = new SimpleStringProperty();
+    private StringProperty linkedAcc = new SimpleStringProperty();
+    private ObjectProperty<TeacherFx> teacherFx = new SimpleObjectProperty<>();
+    private ObjectProperty<StudentFx> studentFx = new SimpleObjectProperty<>();
+
+    public long getUserId() {
+        return userId.get();
+    }
+
+    public LongProperty userIdProperty() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId.set(userId);
+    }
+
+    public String getUsername() {
+        return username.get();
+    }
+
+    public StringProperty usernameProperty() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username.set(username);
+    }
+
+    public String getPasswrd() {
+        return passwrd.get();
+    }
+
+    public StringProperty passwrdProperty() {
+        return passwrd;
+    }
+
+    public void setPasswrd(String passwrd) {
+        this.passwrd.set(passwrd);
+    }
+
+    public String getLinkedAcc() {
+        return linkedAcc.get();
+    }
+
+    public StringProperty linkedAccProperty() {
+        return linkedAcc;
+    }
+
+    public void setLinkedAcc(String linkedAcc) {
+        this.linkedAcc.set(linkedAcc);
+    }
+
+    public TeacherFx getTeacherFx() {
+        return teacherFx.get();
+    }
+
+    public ObjectProperty<TeacherFx> teacherFxProperty() {
+        return teacherFx;
+    }
+
+    public void setTeacherFx(TeacherFx teacherFx) {
+        this.teacherFx.set(teacherFx);
+    }
+
+    public StudentFx getStudentFx() {
+        return studentFx.get();
+    }
+
+    public ObjectProperty<StudentFx> studentFxProperty() {
+        return studentFx;
+    }
+
+    public void setStudentFx(StudentFx studentFx) {
+        this.studentFx.set(studentFx);
+    }
+
+    @Override
+    public String toString() {
+        return "UserFx{" +
+                "userId=" + userId +
+                ", username=" + username +
+                ", passwrd=" + passwrd +
+                ", linkedAcc=" + linkedAcc +
+                ", teacherFx=" + teacherFx +
+                ", studentFx=" + studentFx +
+                '}';
+    }
 }
--- Dziennik/src/main/java/modelFX/WarnsFx.java
+++ Dziennik/src/main/java/modelFX/WarnsFx.java
@@ -1,4 +1,84 @@
 package modelFX;
 
+import javafx.beans.property.*;
+
+import java.time.LocalDate;
+
 public class WarnsFx {
+    private LongProperty warnId = new SimpleLongProperty();
+    private ObjectProperty<LocalDate> dateCreated = new SimpleObjectProperty<>();
+    private StringProperty content = new SimpleStringProperty();
+    private ObjectProperty<StudentFx> studentFx = new SimpleObjectProperty<>();
+    private ObjectProperty<TeacherFx> teacherFx = new SimpleObjectProperty<>();
+
+    public long getWarnId() {
+        return warnId.get();
+    }
+
+    public LongProperty warnIdProperty() {
+        return warnId;
+    }
+
+    public void setWarnId(long warnId) {
+        this.warnId.set(warnId);
+    }
+
+    public LocalDate getDateCreated() {
+        return dateCreated.get();
+    }
+
+    public ObjectProperty<LocalDate> dateCreatedProperty() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(LocalDate dateCreated) {
+        this.dateCreated.set(dateCreated);
+    }
+
+    public String getContent() {
+        return content.get();
+    }
+
+    public StringProperty contentProperty() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content.set(content);
+    }
+
+    public StudentFx getStudentFx() {
+        return studentFx.get();
+    }
+
+    public ObjectProperty<StudentFx> studentFxProperty() {
+        return studentFx;
+    }
+
+    public void setStudentFx(StudentFx studentFx) {
+        this.studentFx.set(studentFx);
+    }
+
+    public TeacherFx getTeacherFx() {
+        return teacherFx.get();
+    }
+
+    public ObjectProperty<TeacherFx> teacherFxProperty() {
+        return teacherFx;
+    }
+
+    public void setTeacherFx(TeacherFx teacherFx) {
+        this.teacherFx.set(teacherFx);
+    }
+
+    @Override
+    public String toString() {
+        return "WarnsFx{" +
+                "warnId=" + warnId +
+                ", dateCreated=" + dateCreated +
+                ", content=" + content +
+                ", studentFx=" + studentFx +
+                ", teacherFx=" + teacherFx +
+                '}';
+    }
 }
