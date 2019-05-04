cc696222cae769d95a05d1a2aee8da8f01ed0eb2
Spanky86 <37106349+Spanky86@users.noreply.github.com>
2019-05-04 19:51:52 +0200

    ScheduleService

---
+++ Dziennik/src/main/java/dao/GradeDao.java
@@ -0,0 +1,52 @@
+package dao;
+
+import Modele.Grades;
+import hibernate.SessionCreator;
+
+import java.util.List;
+
+public class GradeDao  extends SessionCreator implements Dao<Grades>{
+    @Override
+    public void persist(Grades entity) {
+        getCurrentSession().save(entity);
+    }
+
+    @Override
+    public void update(Grades entity) {
+        getCurrentSession().update(entity);
+    }
+
+    @Override
+    public Grades findById(long id) {
+        return (Grades) getCurrentSession().get(Grades.class, id);
+    }
+
+    @Override
+    public void delete(Grades entity) {
+        getCurrentSession().delete(entity);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<Grades> findAll() {
+        return (List<Grades>) getCurrentSession().createQuery("from Grades").list();
+    }
+
+    @Override
+    public void deleteAll() {
+        List<Grades> grades = findAll();
+        grades.forEach(this::delete);
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<Grades> findAllGradesOfStudent(long studnetId){
+        List<Grades> grades = (List<Grades>) getCurrentSession().createQuery("from Grades where student_id = "+studnetId).list();
+        return grades;
+    }
+/*
+    @SuppressWarnings("unchecked")
+    public void deleteById(Student entity, Integer id){
+        getCurrentSession().delete(entity,id);
+    }
+*/
+}
